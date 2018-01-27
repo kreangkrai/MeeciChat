bca6a91a5d75ee1438c13d35cc240232cfa4b3ef
meeci <kreangkrai.ratt@gmail.com>
2018-01-27 15:57:38 +0700

    Alert OK

--- app/src/main/java/mee/contrologic/com/meecichat/fragment/RegisterFragment.java
+++ app/src/main/java/mee/contrologic/com/meecichat/fragment/RegisterFragment.java
@@ -3,19 +3,97 @@ package mee.contrologic.com.meecichat.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.zip.Inflater;
 
+import mee.contrologic.com.meecichat.MainActivity;
 import mee.contrologic.com.meecichat.R;
+import mee.contrologic.com.meecichat.utility.MyAlert;
 
 /**
  * Created by SAMSUNG on 27/1/2561.
  */
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar Controller
+
+        toolbarController();
+
+    }// Main Method
+
+    private void toolbarController() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+//        setup title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register_th));
+
+//        setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+//        setup menu
+        setHasOptionsMenu(true);
+
+    } //Toolbar
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_register,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.itemRegister)
+        {
+            uploadValuetoFirebase();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValuetoFirebase() {
+
+//        Initial View
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtEmail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String emailString = emailEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+        if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty())
+        {
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space","Please fill all every blank");
+        }
+        else
+        {
+
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
@@ -23,4 +101,4 @@ public class RegisterFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_register,container,false);
         return  view;
     }
-}
+}// Main Class
