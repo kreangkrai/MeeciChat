7446c7f2936a141698d6e6dbbbd596ac58fa6fe5
meeci <kreangkrai.ratt@gmail.com>
2018-01-27 10:40:06 +0700

    Initial commit

---
+++ app/src/main/java/mee/contrologic/com/meecichat/MainActivity.java
@@ -0,0 +1,13 @@
+package mee.contrologic.com.meecichat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
