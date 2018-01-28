ac35043efee77f44f52cdcf6563bb89cd5f8a07f
meeci <kreangkrai.ratt@gmail.com>
2018-01-28 14:18:23 +0700

    Add ChatFragment Success

--- app/src/main/java/mee/contrologic/com/meecichat/ServiceActivity.java
+++ app/src/main/java/mee/contrologic/com/meecichat/ServiceActivity.java
@@ -3,11 +3,23 @@ package mee.contrologic.com.meecichat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mee.contrologic.com.meecichat.fragment.ChatFragment;
+import mee.contrologic.com.meecichat.fragment.RegisterFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentServiceFragment,new ChatFragment())
+                    .commit();
+        }
+
+
     }//Main Method
 }// Main Class
